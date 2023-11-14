8591de9943d746089c576ec88fa000f291f7ae3d
Gurkirat Singh <sgurkirat3788@gmail.com>
2023-11-14 18:58:10 +0530

    small changes

--- 4. Objects in JavaScript/42-Objects inside Array.js
+++ 4. Objects in JavaScript/42-Objects inside Array.js
@@ -2,14 +2,14 @@
 // very useful in real world applications
 
 const users =[
-    {userld: 1, firstName: 'harshit', gender: 'male'},
-    {userld: 2, firstName: 'mohit', gender: 'male'},
-    {userld: 3, firstName: 'nitish', gender: 'male '} 
+    {userId: 1, firstName: 'harshit', gender: 'male'},
+    {userId: 2, firstName: 'mohit', gender: 'male'},
+    {userId: 3, firstName: 'nitish', gender: 'male '} 
 ]
 
 console.log(users);
 
 // iterating
 for(let user of users){
-    console.log(user.userld);
+    console.log(user.userId);
 }
